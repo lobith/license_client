98c72b27080875188333922678a150e4b60c9acf
Daniel Walz <developer@foleysfinest.com>
2024-10-01 15:58:58 +0200

    Fixed windows build

--- cmake/Sodium.cmake
+++ cmake/Sodium.cmake
@@ -14,29 +14,39 @@ set(SODIUM_ROOT ${CMAKE_BINARY_DIR}/_deps/libsodium-src)
 set(SODIUM_PATH ${SODIUM_ROOT}/src/libsodium)
 
 if(APPLE)
-    set(SODIUM_BUILD_SCRIPT ${SODIUM_PATH}/dist-build/macos.sh)
-    set(SODIUM_INCLUDE_PATH ${SODIUM_PATH}/libsodium-osx/include)
-    set(SODIUM_LIBRARY_FILE ${SODIUM_PATH}/libsodium-osx/lib/libsodium.a)
-else()
-    set(SODIUM_BUILD_SCRIPT ${CMAKE_COMMAND} -E msbuild ${SODIUM_PATH}/builds/msvc/vs2022/libsodium/libsodium.sln /property:Configuration=${CMAKE_BUILD_TYPE})
-    set(SODIUM_INCLUDE_PATH ${SODIUM_PATH}/libsodium-win/include)
-    set(SODIUM_LIBRARY_FILE ${SODIUM_PATH}/libsodium-win/lib/sodium.lib)
-endif()
 
-ExternalProject_Add(libsodium
+    message(STATUS "Downloading libsodium mac sources")
+
+    set(SODIUM_LIBRARY_FILE ${SODIUM_PATH}/libsodium-osx/lib/libsodium.a)
+
+    ExternalProject_Add(libsodium
         PREFIX ${SODIUM_ROOT}
         URL https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
         CONFIGURE_COMMAND ""
         BUILD_IN_SOURCE TRUE
-        BUILD_COMMAND ${SODIUM_BUILD_SCRIPT}
+        BUILD_COMMAND ${SODIUM_PATH}/dist-build/macos.sh
         BUILD_BYPRODUCTS ${SODIUM_LIBRARY_FILE}
         INSTALL_COMMAND ""
-)
+    )
 
+    add_dependencies(sodium libsodium)
+    set_property(TARGET sodium PROPERTY IMPORTED_LOCATION "${SODIUM_LIBRARY_FILE}")
+    target_include_directories(sodium INTERFACE "${SODIUM_PATH}/libsodium-osx/include")
 
-add_dependencies(sodium libsodium)
+else()
 
-set_property(TARGET sodium PROPERTY
-        IMPORTED_LOCATION "${SODIUM_LIBRARY_FILE}")
+    message(STATUS "Downloading libsodium windows prebuilt")
 
-target_include_directories(sodium INTERFACE "${SODIUM_INCLUDE_PATH}")
+    FetchContent_Declare(download_sodium
+            URL https://download.libsodium.org/libsodium/releases/libsodium-1.0.20-stable-msvc.zip)
+
+    FetchContent_MakeAvailable(download_sodium)
+
+    add_dependencies(sodium download_sodium)
+
+    target_compile_definitions(sodium INTERFACE SODIUM_STATIC)
+    target_include_directories(sodium INTERFACE "${download_sodium_SOURCE_DIR}/include")
+    set_property(TARGET sodium PROPERTY
+            IMPORTED_LOCATION "${download_sodium_SOURCE_DIR}/x64/${CMAKE_BUILD_TYPE}/v143/static/libsodium.lib")
+
+endif()
--- examples/juce_plugin/LicenseData.cpp
+++ examples/juce_plugin/LicenseData.cpp
@@ -7,13 +7,13 @@
 
 namespace LicenseData {
 
-static const unsigned char temp_productName[]   = "Cobra Fuzz";
-static const unsigned char temp_copyright[]     = "Copyright Safari Pedals Ltd. 2022-2024";
-static const unsigned char temp_version[]       = "0.0.3";
-static const unsigned char temp_authServerUrl[] = "https://safari.foleysfinest.com/";
-static const unsigned char temp_buyUrl[]        = "https://safaripedals.com/products/cobra-fuzz-vintage-venomous-fuzz";
-static const unsigned char temp_manualUrl[]     = "https://uc76f4aac19940625201aea31620.dl.dropboxusercontent.com/cd/0/inline2/Ca7FqiFSf49J3Iw1bY4_7qncWx1CLsSXwRtQoor8yFCHJn5aQcRpbrdfA7aK5oLa6x2VbYdD5FfUeEkRXc8lRXuz-FMunUS784WhtjO95_cI9OrGDWdgPCA8wSoy";
-static const unsigned char temp_productUid[]    = "db9fc87be7729becf772509563b5a82f";
+static const char temp_productName[]   = "Cobra Fuzz";
+static const char temp_copyright[]     = "Copyright Safari Pedals Ltd. 2022-2024";
+static const char temp_version[]       = "0.0.3";
+static const char temp_authServerUrl[] = "https://safari.foleysfinest.com/";
+static const char temp_buyUrl[]        = "https://safaripedals.com/products/cobra-fuzz-vintage-venomous-fuzz";
+static const char temp_manualUrl[]     = "https://uc76f4aac19940625201aea31620.dl.dropboxusercontent.com/cd/0/inline2/Ca7FqiFSf49J3Iw1bY4_7qncWx1CLsSXwRtQoor8yFCHJn5aQcRpbrdfA7aK5oLa6x2VbYdD5FfUeEkRXc8lRXuz-FMunUS784WhtjO95_cI9OrGDWdgPCA8wSoy";
+static const char temp_productUid[]    = "db9fc87be7729becf772509563b5a82f";
 static const unsigned char temp_publicKey[] = { 0x09, 0xd0, 0x35, 0xe4, 0xa8, 0xa3, 0x5e, 0x30, 0xf6, 0xd5, 0x52, 0xbf, 0xb0, 0x42, 0xb4, 0x75, 0x11, 0x76, 0x2d, 0x0b, 0x16, 0x0f, 0x77, 0x56, 0x1e, 0x1a, 0x3b, 0x14, 0x18, 0x54, 0x4c, 0x46 };
 static const unsigned char temp_privateKey[] = { 0x02, 0x41, 0x50, 0x06, 0x19, 0xee, 0xd0, 0x21, 0xf8, 0x45, 0xc0, 0x48, 0xb9, 0xaf, 0xea, 0xfd, 0x17, 0x47, 0x2b, 0x4a, 0x24, 0x07, 0x15, 0x03, 0x13, 0x27, 0x6b, 0x24, 0xf2, 0xf6, 0xa0, 0x60 };
 const char* productName   = (const char*) temp_productName;
@@ -23,7 +23,7 @@ const char* authServerUrl = (const char*) temp_authServerUrl;
 const char* buyUrl        = (const char*) temp_buyUrl;
 const char* manualUrl     = (const char*) temp_manualUrl;
 const char* productUid    = (const char*) temp_productUid;
-const char* publicKey     = (const char*) temp_publicKey;
-const char* privateKey    = (const char*) temp_privateKey;
+const unsigned char* publicKey     = (unsigned const char*) temp_publicKey;
+const unsigned char* privateKey    = (unsigned const char*) temp_privateKey;
 
 }
